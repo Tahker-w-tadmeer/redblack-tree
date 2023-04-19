9b77f00dacb20004892a533dfe66b982d9827923
peter anton <97073590+Peter-Anton@users.noreply.github.com>
2023-04-19 13:05:29 +0200

    make clean code

--- new_rbt.py
+++ new_rbt.py
@@ -1,56 +1,9 @@
-class Printable:
-    def display(self):
-        lines, *_ = self._display_aux()
-        for line in lines:
-            print(line)
+from typing import Any
 
-    def _display_aux(self):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if self.right is None and self.left is None:
-            line = '%s' % (self.key + " " + ("B" if self.color == "black" else "R"))
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if self.right is None:
-            lines, n, p, x = self.left._display_aux()
-            s = '%s' % (self.key + " " + ("B" if self.color == "black" else "R"))
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if self.left is None:
-            lines, n, p, x = self.right._display_aux()
-            s = '%s' % (self.key + " " + ("B" if self.color == "black" else "R"))
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.left._display_aux()
-        right, m, q, y = self.right._display_aux()
-        s = '%s' % (self.key + " " + ("B" if self.color == "black" else "R"))
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
+import help_function
 
 
-class Node(Printable):
+class Node(help_function.Printable):
     def __init__(self, value):
         self.color = 'red'
         self.parent = None
@@ -152,7 +105,7 @@ class Red_BLack_Tree:
                     self._left_rotate(n.parent.parent)
         self.root.color = 'black'
 
-    def get_uncle(self, n: Node) -> Node:
+    def get_uncle(self, n: Node) -> Any | None:
         x = n.parent
         if x is None:
             return None
@@ -197,4 +150,3 @@ class Red_BLack_Tree:
                 n = n.right
                 continue
         return None
-
